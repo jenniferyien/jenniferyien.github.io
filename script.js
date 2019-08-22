7561beeeb870f0e2a33247fa63bba8410f555990
Jennifer Yien <jenniferllwu@gmail.com>
2019-08-21 22:24:44 -0400

    adjust styling for mobile and animate scroll

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ $(function(){
   $("#nav").addClass("js").before('<div id="menu">&#9776;</div>');
   $("#menu").click(function(){
 	   $("#nav").toggle();
+     if ($("#menu").hasClass('close')) {
+       $("#menu").removeClass('close').html('&#9776;')
+     } else {
+       $("#menu").addClass('close').html('&#x2717;')
+     }
   });
 
   // resize of window
@@ -14,7 +19,32 @@ $(function(){
   });
   $("#about").click(function() {
     $('html,body').animate({
-        scrollTop: $(".about-section").offset().top
+        scrollTop: $(".about").offset().top
       },'slow');
   });
+  $("#experience").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".experience").offset().top
+      },'slow');
+  });
+  $("#education").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".education").offset().top
+      },'slow');
+  });
+  $("#backUpButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".hero-section").offset().top
+      },'slow');
+  });
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+      $('#backUpButton').fadeIn();
+    } else {
+      $('#backUpButton').fadeOut();
+    }
+  });
+
 });
